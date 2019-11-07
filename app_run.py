168e7eec8005f6cbe1c7e3edf2c234f5cf45fcf7
hannn0919 <dearfriend6258@gmail.com>
2019-11-07 18:19:59 +0800

    update app_run.py

--- app_run.py
+++ app_run.py
@@ -5,41 +5,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
-@app.route('/index')
 def index():
-    data = "Deploying a Flask App To Heroku"
-    return render_template('index.html')
-
-@app.route('/add')
-def add():
-    a = num(request.args.get('a'))
-    b = num(request.args.get('b'))
-    return str(calculator.add(a,b))
-
-@app.route('/minus')
-def minus():
-    a = num(request.args.get('a'))
-    b = num(request.args.get('b'))
-    return str(calculator.minus(a,b))
-
-@app.route('/times')
-def times():
-    a = num(request.args.get('a'))
-    b = num(request.args.get('b'))
-    return str(calculator.times(a,b))
-
-@app.route('/divided')
-def divided():
-    a = num(request.args.get('a'))
-    b = num(request.args.get('b'))
-    return str(calculator.divided(a,b))
+    return "安安"
 
 
-def num(s):
-    try:
-        return int(s)
-    except ValueError:
-        return float(s)
 
 if __name__ == '__main__':
     app.run(debug=True)
